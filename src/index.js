b4c9b26d6589218be0bc225698c7ff5e35e148bd
Gareth <me@gareth.codes>
2017-09-21 00:32:58 -0700

    saving before adding voting features

--- src/index.js
+++ src/index.js
@@ -6,10 +6,22 @@ import registerServiceWorker from './registerServiceWorker'
 import './styles/index.css'
 import { BrowserRouter } from 'react-router-dom'
 import { ApolloProvider, createNetworkInterface, ApolloClient } from 'react-apollo'
+import { GC_AUTH_TOKEN } from './constants'
 
 const networkInterface = createNetworkInterface({
   uri: 'https://api.graph.cool/simple/v1/cj7s5cj3l068o0143z1tvzvke'
 })
+
+networkInterface.use([{
+  applyMiddleware (req, next) {
+    if (!req.options.headers) {
+      req.options.headers = {}
+    }
+    const token = localStorage.getItem(GC_AUTH_TOKEN)
+    req.options.headers.authorization = token ? `Bearer ${token}` : null
+    next()
+  }
+}])
 
 const client = new ApolloClient({
   networkInterface
